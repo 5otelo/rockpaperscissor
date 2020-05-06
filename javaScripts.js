88837e3dbde1ad9c18495ba4973f27787ac00a78
omarmalonso <64847466+omarmalonso@users.noreply.github.com>
2020-05-05 19:53:05 -0500

    Update javaScripts.js

--- javaScripts.js
+++ javaScripts.js
@@ -19,7 +19,25 @@ document.getElementById("rock1").onclick = function(){
 document.getElementById("rock1").onclick = function(){
  player1 = "Scissor";
 }
-document.getElementById("check
+document.getElementById("check") = function(){
 if(player1=="Rock" && player2=="Rock"){
-winner="Tie!"
+winner="Tie! Try again.";
+}else if(player1=="Rock" && player2=="Paper"){
+winner="Player 2 wins!";
+}else if(player1=="Rock" && player2=="Scissor"){
+winner="Player 1 wins!";
+}else if(player1=="Paper" && player2=="Rock"){
+winner="Player 1 wins!";
+}else if(player1=="Paper" && player2=="Paper"){
+winner="Tie! Try again.";
+}else if(player1=="Paper" && player2=="Scissor"){
+winner="Player 2 wins!";
+}else if(player1=="Scissor" && player2=="Rock"){
+winner="Player 2 wins!";
+}else if(player1=="Scissor" && player2=="Paper"){
+winner="Player 1 wins!";
+}else if(player1=="Scissor" && player2=="Scissor"){
+winner="Tie! try again.";
+}
+ document.getElementById("winner").innerHTML=winner;
 }
