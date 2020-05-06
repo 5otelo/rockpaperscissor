1c595758d41d089dfa6f0a6ab1ca3d7203385b7b
omarmalonso <64847466+omarmalonso@users.noreply.github.com>
2020-05-05 19:19:02 -0500

    Update javaScripts.js

--- javaScripts.js
+++ javaScripts.js
@@ -1,31 +1,25 @@
 var winner= "nobody";
-  
-
-  var a = document.getElementById("rockButton").value;
-  var b = document.getElementById("paperButton").value;
-  var c = document.getElementById("scissorButton").value;
-  var x = document.getElementById("rockButton2").value;
-  var y = document.getElementById("paperButton2").value;
-  var z = document.getElementById("scissorButton2").value;
-
-function dosomething(){
-if(val.id == "rockButton" && val.id == "rockButton2"){
-  document.getElementById("winner").innerHTML=winner = "tie";
-}else if(val.id == "rockButton" && val.id == "paperButton2"){
-  document.getElementById("demo").innerHTML="Player 2 Wins!";
-}else if(val.id == "rockButton" && val.id == "scissorsButton2"){
-  document.getElementById("demo").innerHTML="Player 1 Wins!";
-}else if(val.id == "paperButton" && val.id == "paperButton2"){
-  document.getElementById("demo").innerHTML="Tie, play again";
-}else if(val.id == "paperButton" && val.id == "rockButton2"){
-  document.getElementById("demo").innerHTML="Player 1 Wins!";
-}else if(val.id == "paperButton" && val.id == "scissorButton2"){
-  document.getElementById("demo").innerHTML="Player 2 Wins!";
-}else if(val.id == "scissorButton" && val.id == "scissorButton2"){
-  document.getElementById("demo").innerHTML="Tie, play again";
-}else if(val.id == "scissorButton" && val.id == "rockButton2"){
-  document.getElementById("demo").innerHTML="Player 2 Wins!";
-}else if(val.id == "scissorButton" && val.id == "paperButton2"){
-  document.getElementById("demo").innerHTML="Player 1 Wins!";
+var player1 ="";
+var player2 ="";
+document.getElementById("rock1").onclick = function(){
+ player1 = "Rock";
 }
+document.getElementById("rock1").onclick = function(){
+ player1 = "Paper";
+}
+document.getElementById("rock1").onclick = function(){
+ player1 = "Scissor";
+}
+document.getElementById("rock1").onclick = function(){
+ player1 = "Rock";
+}
+document.getElementById("rock1").onclick = function(){
+ player1 = "Paper";
+}
+document.getElementById("rock1").onclick = function(){
+ player1 = "Scissor";
+}
+document.getElementById("check
+if(player1=="Rock" && player2=="Rock"){
+winner="Tie!"
 }
